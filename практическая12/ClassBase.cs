cb9c65f31cf7e00bb3cef79ab8c5e9318fff4d03
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-12-07 16:15:57 +0300

    Добавьте файлы проекта.

---
+++ практическая12/ClassBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практическая12
+{
+    internal class ClassBase
+    {
+        public static Entities Base;
+    }
+}
